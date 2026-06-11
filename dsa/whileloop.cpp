540669e7c2d25b2112b3e74cd9756ada2d162338
Your Name <you@example.com>
2026-06-11 23:22:44 +0530

    commit7

--- dsa/whileloop.cpp
+++ dsa/whileloop.cpp
@@ -32,5 +32,14 @@
 //         i++;
 //     }
 // }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i;
+//     while(i=10){
+//         cout<<i<<endl;
+//         i++;
+//     }
 
-
+//     }
+    
