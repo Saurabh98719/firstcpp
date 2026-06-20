42c213de9e79be428b04cf70823af9ea2beb3ab1
Saurabh98719 <ksaurabh98719@gmail.com>
2026-06-20 16:13:08 +0530

    Add files via upload

---
+++ reversearr.cpp
@@ -0,0 +1,36 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[3] = {2,4,5};
+//     int n = sizeof(arr)/4;
+//    int b[n];
+//     for (int i = 0; i <= n; i++){
+//     b[i] = arr[n-1-i];
+//     cout<<b[i]<<" ";
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[4]= {1,2,3,1};
+//     int b[4];
+//     for (int i = 0; i <= 3; i++){
+//         b[i] = arr[4-1-i];
+//     cout<<b[i]<< " ";
+//     }
+
+// }
+    
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[4]= {1,2,5,9};
+//     int b[4];
+//     for (int i = 0; i <= 3; i++){
+//         b[i] = arr[4-1-i];
+//     cout<<b[i]<< " ";
+//     }
+
+// }
+    
