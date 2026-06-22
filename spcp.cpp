43df79d1d2f6076199f9becb9dfe88ff9201768d
Saurabh98719 <ksaurabh98719@gmail.com>
2026-06-22 21:13:32 +0530

    Add files via upload

---
+++ spcp.cpp
@@ -0,0 +1,47 @@
+// include<iostream>
+// using namespace std;
+//   int main() {
+//     int cp;
+//     cout<<" enter cp value : ";
+//     cin>>cp;
+//     int sp;
+//     cout<<" enter sp value : ";
+//     cin>>sp;
+//     if(sp>cp){
+//         cout<<" profit" <<sp-cp;
+
+//     }
+//     if(sp==cp){
+//         cout<<" no profit no loss";
+//     }
+//     else{
+//         cout<<" loss" <<cp-sp;
+
+//     }
+// //  }*
+// #include<iostream>
+// using namespace std;    
+// int main(){
+//     int n;
+//     cin>>n;
+//     if(n>99&&n<1000){
+//         cout<<"three digit number";
+//     }
+//     else{
+//         cout<<"not three digit number";
+//     } 
+// }
+
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if (n%2==0 and n%3==0){
+        cout<<"divisible by 2 and3";
+    }
+    else {
+        cout<<"not divisible by 2 and 3";
+    }
+
+}
