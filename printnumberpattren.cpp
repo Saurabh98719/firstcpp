c763b5f39805e5d366e703f4921bac54167be825
Saurabh98719 <ksaurabh98719@gmail.com>
2026-06-25 02:27:27 +0530

    Add files via upload

---
+++ printnumberpattren.cpp
@@ -0,0 +1,67 @@
+// // #include<iostream>
+// // using namespace std;
+// // int main(){
+// //     int n;
+// //     cout<<"enter rows: " ;
+// //     cin>>n;
+// //     for(int i=1;i<=n;i++){
+// //         cout<<"1 2 3 4 "<<endl;
+// //     }
+// // }
+
+// // #include<iostream>
+// // using namespace std;
+// // int main(){
+// //     cout<<"1 2 3 4 "<<endl;
+// //     cout<<"1 2 3 4 "<<endl;
+// //     cout<<"1 2 3 4 "<<endl;
+// //     cout<<"1 2 3 4 "<<endl;
+// // }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter rows: " ;
+//     cin>>n;
+//     int m;
+//     cout<<"enter column: " ;
+//     cin>>m;
+//     for(int i=1; i<=n;i++){
+//         for(int j=1; j<=m;j++){
+//             cout<< j <<" "<<endl;
+        
+//         }
+    
+//     }
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter rows: " ;
+//     cin>>n;
+//     int m;
+//     cout<<"enter column: " ;
+//     cin>>m;
+//     for(int i=0; i<=n;i++){
+//         for(int j=0; j<=m;j++){
+//             cout << "a b c d"<<endl;
+//         }
+//     }
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter rows: " ;
+//     cin>>n;
+//     int m;
+//     cout<<"enter column: " ;
+//     cin>>m;
+//     for(int i=1; i<=n;i++){
+//         for(int j=1; j<=m;j++){
+//         cout<<"* "<<endl ;
+//         }
+//     }
+// }
