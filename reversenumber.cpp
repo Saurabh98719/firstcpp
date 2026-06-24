b2cb89ee33365b3e39736867776e97e57db28ade
Saurabh98719 <ksaurabh98719@gmail.com>
2026-06-24 21:37:02 +0530

    Add files via upload

---
+++ reversenumber.cpp
@@ -0,0 +1,34 @@
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     for(int i =100; i>=1;i--){
+//         cout<<i<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     for(int i =100; i>=1;i-=2){
+//         cout<<i<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<" enter a number: ";
+//     cin>>n;
+//     for(int i =n; i>=1;i-=10){
+//         cout<<i<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;    
+// int main(){
+// for(int i=10; i>=1;i--){
+//     cout<<i <<" ";
+// }
+// }
---
+++ scopeofvariable.cpp
@@ -0,0 +1,20 @@
+// #include<iostreaM>
+// using namespace std;
+// int main (){
+//     int n;
+//     cout<<" enter value of n: ";
+//     cin>>n;
+//     for(int i= 1; i<=n;i++)
+//     cout<<i<<endl;
+// }
+
+// #include<iostreaM>
+// using namespace std;
+// int main (){
+//     int n;
+//     cout<<" enter value of n: ";
+//     cin>>n;
+//     for(int i= 1; i<=n;i++)
+//     cout<<i<<endl;
+//     cout<<i<<endl;
+// }
