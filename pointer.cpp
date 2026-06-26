faf13104dc7b309a7890ffb74d2b6d3c998523d4
Saurabh98719 <ksaurabh98719@gmail.com>
2026-06-26 16:54:31 +0530

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,35 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a= 3;
+//     cout<<&a<<endl;
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x = 4;
+//     int y=7;
+//     cout<<&x<<endl;
+//     cout<<&y<<endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x = 5;
+//     int*ptr=&x;
+//     cout<<&x;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 8;
+//     int*ptr=&a;
+//     cout<<a<<endl;
+//     cout<<&a<<endl;
+//     cout<<*ptr<<endl;
+//     cout<<&ptr<<endl;
+// }
---
+++ print18thtable.cpp
@@ -0,0 +1,30 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=18;i<=180;i = i+18){
+//         cout<<i<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;    
+// int main (){
+//     for(int i=180;i>=18;i = i-18){
+//         cout<<i<<endl;
+//     }
+// }
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     for(int i=18;i<=180;i = i+18){
+//        cout<<i<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     for(int i=18;i<=180;i = i+18){
+//          if(i%18 == 0) cout<<i<<" "<<endl;
+//     }
+// }
