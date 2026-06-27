71e08a43377dee655a2250a6bf8f027f959f87b0
Saurabh98719 <ksaurabh98719@gmail.com>
2026-06-27 16:43:55 +0530

    Add files via upload

---
+++ incrementdecrement.cpp
@@ -0,0 +1,50 @@
+// #include<iostream>
+// using namespace std; 
+// int main() {
+//      int x = 3;
+//      cout << x<<endl;
+//      x+= 2;
+//         cout << x<<endl;
+// }
+
+// #include<iostream>
+// using namespace std; 
+// int main() {
+//      int a = 5;
+//      cout<<a<<endl;
+//      a-= 3;
+//      cout <<a<<endl;
+// }
+
+// #include<iostream>
+// using namespace std; 
+// int main() {
+//     int a = 5;
+//     cout << a << endl;
+//     a++;
+//     cout << a << endl;
+//     a--;
+//     cout << a << endl;
+//     ++a;
+//     cout<<a<<endl;
+//     --a;
+//     cout << a << endl; 
+// }
+
+// #include<iostream>
+// using namespace std; 
+// int main() {
+//     int x= 5;
+//     cout <<x<< endl;
+//     cout <<x++ <<endl;
+//     cout<<x<<endl;
+// }
+
+// #include<iostream>
+// using namespace std; 
+// int main() {
+//     int x = 5;
+//     cout <<x<<endl;
+//     cout<<++x<<endl;
+//     cout<<x<<endl;
+// }
---
+++ libraryfunction.cpp
@@ -0,0 +1,37 @@
+// #include<iostreaM>
+// #include<math.h>
+// using namespace std;
+// int main (){
+//     cout<<sqrt(9);
+    
+// }
+
+// #include<iostreaM>
+// #include<math.h>
+// using namespace std;
+// int main (){
+//     cout<<cbrt(8.0065);
+    
+// }
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main(){
+//     cout<<max(11.0022,11.0023);
+// }
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main(){
+//     cout<<min(-56,-45);
+// }
+
+// #include<iostreaM>
+// #include<math.h>
+// using namespace std;
+// int main (){
+//     cout<<pow(2,9);
+    
+// }
