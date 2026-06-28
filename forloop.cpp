50efa5c5c75fff321269132a6b3785b6445e7601
Saurabh98719 <ksaurabh98719@gmail.com>
2026-06-28 20:27:25 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,33 @@
+// // #include<iostream>
+// // using namespace std;
+// // int main() {
+// //     for (int i = 1; i <= 10; i++) {
+// //         cout << i << endl;
+// //     }
+
+// // #include<iostream>
+// // using namespace std;
+// // int main() {
+// //     for (int i = 5; i <= 10; i++) {
+// //         cout << i << endl;
+// //     }
+// // }
+
+// // #include<iostream>
+// // using namespace std;
+// // int main() {
+// //     for (int i = 10; i >= 1; i--) {
+// //         cout << i << endl;
+// //     }
+ // }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<"enter number: ";
+//     cin>>n;
+//     for (int i = 1; i<= n; i++) {
+//         cout << i << endl;
+//     }
+// }
